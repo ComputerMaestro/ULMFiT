ec70056ee2370ce63656f9c796ba12f6cdcf5626
Yash Patel <yp270200@gmail.com>
2019-07-08 07:57:53 +0530

    forward function changed

--- pretrainLM.jl
+++ pretrainLM.jl
@@ -65,45 +65,33 @@ function generator(c::Channel, corpus; batchsize::Integer=64, bptt::Integer=70)
     end
 end
 
-# Computes output of the layer for given batch
-function compute_layer(layer, batch::AbstractArray)
-    gpu!(layer)
-    batch = broadcast(x -> cpu(layer(gpu(x))), batch)
-    cpu!(layer)
-    return batch
+function forward(model_layers, x, lm, h_prev, α, β, ar_value, tar_value)
+    x = gpu(model_layers[1](indices(x, lm.vocab, "_unk_")))
+    x = model_layers[2:7](x)
+    h_prev == nothing || (tar_value += β*sum(norm, h_prev .- x))    #calculates TAR value (regularization)
+    h_prev = copy(x)
+    x = model_layers[8](x)
+    ar_value += α*sum(norm, x)  # Calculates AR value (regularization)
+    x = model_layers[9:end](x)
+    return x, h_prev, ar_value, tar_value
+end
+
+# loss funciton - Loss calculation with AR and TAR regulatization
+function loss(X, Y, lm)
+    l, ar_value, tar_value = 0.0f0, 0.0f0, 0.0f0
+    h_prev = nothing
+    for x, y in zip(X, Y)
+        x, h_prev, ar_value, tar_value = forward(model_layers, x, lm, h_prev, α, β, ar_value, tar_value)
+        y = indices(y, lm.vocab, "_unk_")
+        y = gpu(Flux.onehotbatch(y, lm.vocab, "_unk_"))
+        l += sum(crossentropy(x, y))
+    end
+    Flux.truncate!(lm)
+    return l+ar_value+tar_value
 end
 
 # Gradient Clipping
 grad_clipping(g, upper_bound) = min(g, upper_bound)
-
-# Forward pass
-function forward(model_layers, batch::AbstractVector, lm::LanguageModel, batchsize::Integer, H_prev::AbstractArray, α, β)
-    reset_masks!.(model_layers)
-    batch = broadcast(x -> indices(x, lm.vocab, "_unk_"), batch)
-    gpu!(lm.embedding_layer)
-    batch = broadcast(x -> cpu(lm.embedding_layer(x)), batch)
-    cpu!(lm.embedding_layer)
-    for layer in model_layers
-        compute_layer(layer, batch)
-    end
-    gpu!(lm.embedding_layer)
-    batch = broadcast(x -> cpu(softmax(lm.embedding_layer(gpu(x), true))), batch)
-    cpu!(model_layers[1])
-
-    Y = take!(gen)
-    Y = broadcast(y -> Flux.onehotbatch(y, lm.vocab, "_unk_"), Y)
-    l = loss(batch, Y, H_prev, α, β)
-
-    return l, Tracker.data.(batch)
-end
-
-# loss funciton - Loss calculation with AR and TAR regulatization
-function loss(H, Y, H_prev, α, β)
-    expr(ht, yt, ht_prev) = sum(crossentropy(ht, yt)) + α*sum(norm, ht) + β*sum(norm, ht .- ht_prev)
-    l = sum(broadcast((ht, yt, ht_prev) -> cpu(expr(gpu(ht), gpu(yt), gpu(ht_prev))), batch, Y, H_prev))
-    Flux.truncate!(lm)
-    return l
-end
 
 # Backward
 function back!(p::Flux.Params, l, opt, gradient_clip::Float64)
@@ -114,11 +102,11 @@ function back!(p::Flux.Params, l, opt, gradient_clip::Float64)
 end
 
 # Funciton for training Language Model
-function fit!(lm::LanguageModel; batchsize::Integer=64, bptt::Integer=70,gradient_clip::Float64=0.25,
+function fit!(lm::LanguageModel; batchsize::Integer=64, bptt::Integer=70, gradient_clip::Float64=0.25,
     base_lr=0.004, epochs::Integer=1, α::Number=2, β::Number=1, checkpoint_iter::Integer=5000)
 
     # All important layers for training
-    model_layers = [
+    model_layers = Chain(
         lm.embedding_layer,
         VarDrop((size(lm.embedding_layer.emb, 2), batchsize), lm.wordDropProb),
         lm.lstm_layer1,
@@ -126,28 +114,33 @@ function fit!(lm::LanguageModel; batchsize::Integer=64, bptt::Integer=70,gradien
         lm.lstm_layer2,
         VarDrop((size(lm.lstm_layer2.layer.cell.h, 1), batchsize), lm.LayerDropProb),
         lm.lstm_layer3,
-        VarDrop((size(lm.lstm_layer3.layer.cell.h, 1), batchsize), lm.FinalDropProb)
-    ]
+        VarDrop((size(lm.lstm_layer3.layer.cell.h, 1), batchsize), lm.FinalDropProb),
+        x -> lm.embedding_layer(x, true),
+        softmax
+    )
 
     # Initializations
     gen = Channel(x -> generator(x, loadCorpus(); batchsize = batchsize, bptt = bptt))
-    opt = ADAM(0.004, (0.7, 0.99))    # ADAM Optimizer
+    opt = ADAM(base_lr, (0.7, 0.99))    # ADAM Optimizer
     num_of_batches = take!(gen) # Number of mini-batches
     T = Int(floor((num_of_batches*2)/100))   # Averaging Trigger
-    set_trigger!.(model_layers[[3, 5, 7]])  # Setting triggers for AWD_LSTM layers
+    set_trigger!.(T, model_layers[[3, 5, 7]])  # Setting triggers for AWD_LSTM layers
     p = params(lm)
-    H_prev = repeat([zeros(Float32, length(lm.vocab), batchsize)], bptt)
 
     # Pre-Training loops
     for epoch=1:epochs
         println("\nEpoch: $epoch")
         for i=1:num_of_batches
 
-            # FORWARD
-            l, H_prev = forward(model_layers, take!(gen), lm, batchsize, H_prev, α, β)
+            # FORWARD PROPAGATION
+            X = take!(gen)
+            Y = take!(gen)
+            for layer in model_layers[1:8]
+                gpu!(layer)
+            end
+            l = loss(X, Y, lm)
 
-            # BACKWARD
-            gpu!(lm)
+            # BACK PROPAGATION
             back!(p, l, opt, gradient_clip)
 
             # ASGD Step, after Triggering
@@ -217,14 +210,14 @@ function sampling(starting_text::String, lm::LanguageModel=load_model!())
     word_indices = indices(tokens, lm.vocab, "_unk_")
     embeddings = lm.embedding_layer.(word_indices)
     h = (model_layers.(embeddings))[end]
-    probabilities = softmax(tiedEmbeddings(h, lm.embedding_layer))
+    probabilities = softmax(lm.embedding_layer(h, true)))
     prediction = lm.vocab[findall(isequal(maximum(probabilities)), probabilities)[1]]
     println("SAMPLING...")
     print(prediction, ' ')
 
     while true
         h = model_layers(h)
-        probabilities = softmax(tiedEmbeddings(h, lm.embedding_layer))
+        probabilities = softmax(lm.embedding_layer(h, true)))
         prediction = lm.vocab[findall(isequal(maximum(probabilities)), probabilities)[1]]
         print(prediction, ' ')
         prediction == "_pad_" && break
